a818d4311bb30a67d6752c7b7006ba3aeae9cea3
keweizhan <56283830+keweizhan@users.noreply.github.com>
2025-04-30 20:49:16 -0700

    Set up SpringSecurity Basic Content

---
+++ myproject-backend/src/main/java/com/example/entity/RestBean.java
@@ -0,0 +1,21 @@
+package com.example.entity;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter;
+
+public record RestBean<T>(int code, T data, String msg ) {
+    public static <T> RestBean<T> success(T data) {
+        return new RestBean<>(200, data, "Request Success");
+    }
+
+    public static <T> RestBean<T> success(){
+        return success(null);
+    }
+
+    public static <T> RestBean<T> fail(int code, String msg) {
+        return new RestBean<>(code, null, msg);
+    }
+    public String asJsonString() {
+        return JSONObject.toJSONString(this, JSONWriter.Feature.WriteNulls);
+    }
+}
